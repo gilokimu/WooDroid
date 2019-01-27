f7132752430c1c44b98208199bacd7eceb41427a
Gilbert Kimutai <gilbertkimutai@gmail.com>
2019-01-27 09:16:52 +0300

    setup keys and product retrieve test passed

--- app/src/main/java/me/gilo/wc_app/MainActivity.kt
+++ app/src/main/java/me/gilo/wc_app/MainActivity.kt
@@ -4,9 +4,15 @@ import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import me.gilo.woodroid.Woocommerce
 import me.gilo.woodroid.Woocommerce.Builder
 import me.gilo.woodroid.callback.Status
+import me.gilo.woodroid.models.Product
+import java.util.ArrayList
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +22,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val woocommerce = Builder()
-            .setSiteUrl("")
-            .setApiVersion("")
-            .setConsumerKey("")
-            .setConsumerSecret("")
+        val woocommerce = Woocommerce.Builder()
+            .setSiteUrl("http://157.230.131.179")
+            .setApiVersion("2")
+            .setConsumerKey("ck_26c61abd7eeff238d87dc56585bf26cb2d1a1ec3")
+            .setConsumerSecret("cs_062e8e3a7ae0ce08fdebc0c39f8f834d5e87598e")
             .build()
 
-        woocommerce.products.
+
+        woocommerce.products.enqueue(object : Callback<ArrayList<Product>> {
+            override fun onResponse(call: Call<ArrayList<Product>>, response: Response<ArrayList<Product>>) {
+                val products = response.body()
+                for (product in products!!) {
+                    tvText.append(product.title)
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<Product>>, t: Throwable) {
+
+            }
+        })
     }
 }
