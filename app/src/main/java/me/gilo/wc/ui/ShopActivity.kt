3a56451483e08cd4b9759d29b080ff3d17538890
bitsnaps <bitsnaps@yahoo.fr>
2019-02-16 21:51:06 +0100

    Make WooCommerce REST-api version a variable

--- app/src/main/java/me/gilo/wc/ui/ShopActivity.kt
+++ app/src/main/java/me/gilo/wc/ui/ShopActivity.kt
@@ -42,7 +42,7 @@ class ShopActivity : BaseActivity() {
     private fun products() {
         val woocommerce = Woocommerce.Builder()
             .setSiteUrl("http://157.230.131.179")
-            .setApiVersion("2")
+            .setApiVersion(Woocommerce.API_V2)
             .setConsumerKey("ck_26c61abd7eeff238d87dc56585bf26cb2d1a1ec3")
             .setConsumerSecret("cs_062e8e3a7ae0ce08fdebc0c39f8f834d5e87598e")
             .build()
