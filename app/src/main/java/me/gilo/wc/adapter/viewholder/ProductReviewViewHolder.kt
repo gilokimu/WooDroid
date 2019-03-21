3134e59f4ce6a9413399e599ae42b4c7dfa8294b
Gilbert Kimutai <gilbertkimutai@gmail.com>
2019-03-21 06:37:43 +0300

    Added images on reviews section

--- app/src/main/java/me/gilo/wc/adapter/viewholder/ProductReviewViewHolder.kt
+++ app/src/main/java/me/gilo/wc/adapter/viewholder/ProductReviewViewHolder.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.text.Html
 import android.view.View
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import me.gilo.wc.R
 import me.gilo.wc.ui.product.ShopActivity
 import me.gilo.wc.utils.DateUtils
@@ -21,12 +23,17 @@ class ProductReviewViewHolder(val context: Context, itemView: View) :
         val tvDate = itemView.findViewById<TextView>(R.id.tvDate)
         val rbRating = itemView.findViewById<RatingBar>(R.id.rbRating)
         val tvMessage = itemView.findViewById<TextView>(R.id.tvMessage)
+        val ivImage = itemView.findViewById<ImageView>(R.id.ivImage)
 
         tvName.text = review.reviewer
         tvMessage.text = Html.fromHtml(review.review)
 
         rbRating.rating = review.rating.toFloat()
         tvDate.text = DateUtils.getDateString_shortAndSmart(review.date_created)
+
+        if (review.reviewer_avatar_urls["96"]!!.isNotEmpty()){
+            Picasso.with(context).load(review.reviewer_avatar_urls["96"]).into(ivImage)
+        }
 
     }
 
