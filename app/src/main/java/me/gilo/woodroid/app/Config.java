2cc68830e41efeb758f1d2dfb24d9313abce8f5f
Gilbert Kimutai <gilbertkimutai@gmail.com>
2019-08-14 06:42:17 +0300

    added working demo to test

--- app/src/main/java/me/gilo/woodroid/app/Config.java
+++ app/src/main/java/me/gilo/woodroid/app/Config.java
@@ -3,8 +3,14 @@ package me.gilo.woodroid.app;
 public class Config {
     public static String CURRENCY_SYMBOL = "Ksh.";
 
-    public static String siteUrl = "http://192.168.100.61/shop/index.php";
-    public static String consumerKey = "ck_da34628a4a69128001876492b842be4cd3c76bf8";
-    public static String consumerSecret = "cs_c3c70248288a60bc21a0cddc81acdc018df1632d";
+    public static String siteUrl = "https://gilo.me/store"; //sometimes you need to append /index.php at the end
+    public static String consumerKey = "ck_4ff858c97e1ed097cc24cb95593b2bcc45e835d5";
+    public static String consumerSecret = "cs_24865d302c443dec4a1391e5d2a438ba46e95cb6";
+
+    //sorry guys :( someday I am gonna add live and debug to have this stored somewhere
+    //for now commenting will do
+//    public static String siteUrl = "http://192.168.100.61/shop/index.php";
+//    public static String consumerKey = "ck_da34628a4a69128001876492b842be4cd3c76bf8";
+//    public static String consumerSecret = "cs_c3c70248288a60bc21a0cddc81acdc018df1632d";
 
 }
